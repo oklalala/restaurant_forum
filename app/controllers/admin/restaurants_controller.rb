f16de927b5facc88310751882f3f3c35d2d36f48
Tony Lin <tonylin1208@gmail.com>
2017-11-30 10:37:47 +0800

    regit again by iterm2

---
+++ app/controllers/admin/restaurants_controller.rb
@@ -0,0 +1,57 @@
+class Admin::RestaurantsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :authenticate_admin
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @restaurants = Restaurant.page(params[:page]).per(10)
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      flash[:notice] = "restaurant was successfully created"
+      redirect_to admin_restaurants_path
+    else
+      flash.now[:alart] = "restaurant was failed to create"
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @restaurant.update(restaurant_params)
+      redirect_to admin_restaurant_path(@restaurant)
+      flash[:notice] = "restaurant was successfully updated"
+    else
+      render :edit
+      flash[:alert] = "restaurant was failed to update"
+    end
+  end
+
+  def destroy
+    @restaurant.destroy
+    redirect_to admin_restaurants_path
+    flash[:alert] = "restaurant was deleted"
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :tel_no, :address, :opening_hours, :description, :image)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+end

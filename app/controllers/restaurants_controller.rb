f21a6764e8e1c47b959cb39b70d060c16e2d57bf
Tony Lin <tonylin1208@gmail.com>
2017-12-11 13:36:13 +0800

    add show page for public users

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,6 +2,10 @@ class RestaurantsController < ApplicationController
 
 
   def index
-    
+    @restaurants = Restaurant.page(params[:page]).per(9)
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
   end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
-  validates_presence_of :name
+  validates_presence_of :category_id, :name, :tel_no, :address, :opening_hours, :description
   mount_uploader :image, PhotoUploader
 
   belongs_to :category, optional: true

5299ba1c25b889f376d2ec97a6efcea2ac925c62
Tony Lin <tonylin1208@gmail.com>
2017-12-23 18:05:28 +0800

    commented restaurant's showing image update

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   mount_uploader :avatar, AvatarUploader
-  has_many :comments
+  
+  has_many :comments #原本的指令
+  has_many :restaurants, through: :comments
+  
 
   def admin?
     self.role == "admin"

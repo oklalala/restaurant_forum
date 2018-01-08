59770e72b6bd372e46ae59e99c182888b2e301d1
Tony Lin <tonylin1208@gmail.com>
2018-01-08 22:37:43 +0800

    add unfollow button and controller

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -10,4 +10,13 @@ class FollowshipsController < ApplicationController
       redirect_back(fallback_location: root_path)
     end
   end
+
+  def destroy
+    @followship = current_user.followships.where(following_id: params[:id]).first
+    #不能用find 因為find是找主鍵，following_id不是主鍵 用.first是因為where回傳的是集合
+    @followship.destroy
+    #若不用.first 需要用destroy_all
+    flash[:alert] = "#{@followship.following.email} destroyed"
+    redirect_back(fallback_location: root_path)
+  end
 end
